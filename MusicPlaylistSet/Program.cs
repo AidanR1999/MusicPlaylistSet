dfeba6fa2895de0e52b4f2ea99395c15cd95841e
James Lawn <jl1888@live.co.uk>
2019-02-07 14:33:54 +0000

    premium and n/premium playlist

--- MusicPlaylistSet/Program.cs
+++ MusicPlaylistSet/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -125,6 +126,7 @@ namespace MusicPlaylistSet
                 songsInList++;
                 if (playlistNum == playlist.Key)
                 {
+<<<<<<< HEAD
                     if (customer.IsPremium.Equals(false))
                     {
                         if (songsInList == 100)
@@ -145,6 +147,24 @@ namespace MusicPlaylistSet
                     }
                 }
                 
+=======
+                    //check the number of songs in the playlist and the status of premium
+                    if (playlist.Value.Songs.Count >= 100 && !customer.IsPremium)
+                    {
+                        //display message to the user
+                        Console.WriteLine("Playlist reached capacity. Please upgrade account.");
+                        Console.WriteLine("Press any key to return to the Main Menu.");
+                        Console.ReadKey();
+                        Console.Clear();
+                        MainMenu();
+                    }
+
+                    playlist.Value.Songs.Add(songToAdd);
+                    Console.Clear();
+                    MainMenu();
+                    break;
+                }
+>>>>>>> dd458ba84126536947f16a34091d53c3e58e5016
             }
         }
 
@@ -199,9 +219,103 @@ namespace MusicPlaylistSet
                 Console.WriteLine($"{i}. {playlist.Value.Name}");
                 i++;
             }
+            if (customer.IsPremium)
+            {
+                Console.WriteLine($"{i}. Access Premium Features");
+            }
+        }
+
+        private static void showPlaylistsNoExtras()
+        {
+            int i = 1;
+            Console.WriteLine("0. Back to main menu");
+            foreach (KeyValuePair<int, Playlist> playlist in customer.Library)
+            {
+                Console.WriteLine($"{i}. {playlist.Value.Name}");
+                i++;
+            }
         }
 
         private static void selectPlaylist()
+        {
+            string playlistNumString = Console.ReadLine();
+            int playlistNum = Int32.Parse(playlistNumString);
+
+            if (playlistNum == 0)
+            {
+                Console.Clear();
+                MainMenu();
+            }
+            if (playlistNum == customer.Library.Count + 1 && customer.IsPremium)
+            {
+                Console.Clear();
+                displayPlaylistExtraOptions();
+
+                switch (getUserChoiceExtraPlaylistOption())
+                {
+                    //exit to menu
+                    case 0:
+                        Console.Clear();
+                        showPlaylists();
+                        selectPlaylist();
+                        break;
+                    //unionise
+                    case 1:
+                        Console.Clear();
+                        showPlaylistsNoExtras();
+                        Console.WriteLine("Select 2 playlists to unionise");
+                        createPlaylist(unionisePlaylists(getPlaylist(), getPlaylist()));
+                        break;
+                    //differentiate
+                    case 2:
+                        Console.Clear();
+                        showPlaylistsNoExtras();
+                        Console.WriteLine("Select 2 playlists to intersect");
+                        //createPlaylist(playlistCompliment(getPlaylist(), getPlaylist()));
+                        break;
+                    //intersect
+                    case 3:
+                        Console.Clear();
+                        showPlaylistsNoExtras();
+                        Console.WriteLine("Select 2 playlists to intersect");
+                        createPlaylist(intersectPlaylists(getPlaylist(), getPlaylist()));
+                        break;
+                    //subset of
+                    case 4:
+                        Console.Clear();
+                        showPlaylistsNoExtras();
+                        Console.WriteLine("Select subset playlist first, then superset");
+                        Console.WriteLine(isSubSet(getPlaylist(), getPlaylist()));
+                        break;
+                    //superset of
+                    case 5:
+                        Console.Clear();
+                        showPlaylistsNoExtras();
+                        Console.WriteLine("Select superset playlist first, then subset");
+                        Console.WriteLine(isSuperSet(getPlaylist(), getPlaylist()));
+                        break;
+                    default:
+                        Console.WriteLine("Error: Enter one of the numbers available");
+                        break;
+                }
+            }
+
+            foreach (KeyValuePair<int, Playlist> playlist in customer.Library)
+            {
+                if (playlistNum == playlist.Key)
+                {
+                    displaySongsInPlaylist(playlist.Value);
+                    
+                }
+                if (playlistNum < playlist.Key)
+                {
+                    Console.WriteLine("Error: playlist does not exist");
+                    selectPlaylist();
+                }
+            }
+        }
+
+        private static Playlist getPlaylist()
         {
             string playlistNumString = Console.ReadLine();
             int playlistNum = Int32.Parse(playlistNumString);
@@ -216,7 +330,8 @@ namespace MusicPlaylistSet
             {
                 if (playlistNum == playlist.Key)
                 {
-                    displaySongsInPlaylist(playlist.Value);
+                    return playlist.Value;
+
                 }
                 if (playlistNum < playlist.Key)
                 {
@@ -224,6 +339,39 @@ namespace MusicPlaylistSet
                     selectPlaylist();
                 }
             }
+
+            return null;
+        }
+
+        private static int getUserChoiceExtraPlaylistOption()
+        {
+            string userChoiceStr = Console.ReadLine();
+
+            try
+            {
+                int userChoice = Int32.Parse(userChoiceStr);
+
+                return userChoice;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: Enter a whole number");
+                getUserChoiceExtraPlaylistOption();
+            }
+
+            return 0;
+        }
+
+        private static void displayPlaylistExtraOptions()
+        {
+            Console.WriteLine("0. Back");
+            Console.WriteLine("1. Unionise Playlists ");
+            Console.WriteLine("2. Differentiate Playlists ");
+            Console.WriteLine("3. Intersect Playlists ");
+            Console.WriteLine("4. Check if subset of");
+            Console.WriteLine("5. Check if superset of");
+
         }
 
         private static void displaySongsInPlaylist(Playlist playlist)
@@ -311,6 +459,171 @@ namespace MusicPlaylistSet
 
             Console.Clear();
             MainMenu();
+        }
+
+        private static void createPlaylist(Playlist playlist)
+        {
+            Console.WriteLine("Name: ");
+            playlist.Name = Console.ReadLine();
+
+            customer.Library.Add(customer.Library.Count + 1, playlist);
+
+            Console.Clear();
+            MainMenu();
+        }
+
+        /// <summary>
+        /// Finds all songs two inputted Playlists have in common and returns new Playlist with only matching songs.
+        /// </summary>
+        /// <param name="playlist1">Playlist</param>
+        /// <param name="playlist2">Playlist</param>
+        /// <returns>Playlist intersecting</returns>
+        public static Playlist intersectPlaylists(Playlist playlist1, Playlist playlist2)
+        {
+            //Local Variables.
+            int i = 1;
+            Playlist intersecting = new Playlist();
+            HashSet<string> a = new HashSet<string>();
+            HashSet<string> b = new HashSet<string>();
+
+            //Adding each Songs name from parameter variable playlist1 into Hashset of strings.
+            foreach (Song s in playlist1.Songs)
+            {
+                a.Add(s.Name);
+            }
+
+            //Adding each Songs name from parameter variable playlist2 into Hashset of strings.
+            foreach (Song s in playlist2.Songs)
+            {
+                b.Add(s.Name);
+            }
+
+            //Refactors local variable a to contain only song names (Strings) that are present in both local Variables a and b.
+            a.IntersectWith(b);
+
+            //Loops through every song name (String) in Hashset a.
+            foreach (string s in a)
+            {
+                //Adds Songs to the HashSet<Songs> in local Variable intersecting using i as Id, and s as Name.
+                intersecting.Songs.Add(new Song(i, s));
+
+                //Increases value of i for next iteration.
+                i++;
+            }
+            //Returns local variable intersecting of type Playlist.
+            return intersecting;
+        }
+
+        /// <summary>
+        /// Merges two playlists while deleting duplicates
+        /// </summary>
+        /// <param name="playlist1"></param>
+        /// <param name="playlist2"></param>
+        /// <returns> Playlist unionReturn </returns>
+        public static Playlist unionisePlaylists(Playlist playlist1, Playlist playlist2)
+        {
+            int i = 0;
+            // Create a hashset to store the unionised playlist & set it to hold the first playlist
+            HashSet<Song> union = new HashSet<Song>(playlist1.Songs);
+
+            // Merges the union playlist with playlist2 while deleting duplicates
+            union.UnionWith(playlist2.Songs);
+
+            // Create a new hashset of strings to hold the song names from the union playlist
+            HashSet<string> a = new HashSet<String>();
+            foreach (Song s in union)
+            {
+                a.Add(s.Name);
+            }
+
+            // Create an instance of the playlist class
+            Playlist unionReturn = new Playlist();
+
+            // Loop that adds the songs to the playlist 
+            foreach (string s in a)
+            {
+                unionReturn.Songs.Add(new Song(i, s));
+                i++;
+            }
+            // return the merged playlist
+            return unionReturn;
+        }
+
+        public static bool isSuperSet(Playlist playlist1, Playlist playlist2)
+        {
+            HashSet<string> a = new HashSet<string>();
+            foreach (Song s in playlist1.Songs)
+            {
+                a.Add(s.Name);
+            }
+
+            HashSet<string> b = new HashSet<string>();
+            foreach (Song s in playlist2.Songs)
+            {
+                b.Add(s.Name);
+            }
+
+            return a.IsSupersetOf(b);
+        }
+
+        public static bool isSubSet(Playlist playlist1, Playlist playlist2)
+        {
+            HashSet<string> a = new HashSet<string>();
+            foreach (Song s in playlist1.Songs)
+            {
+                a.Add(s.Name);
+            }
+
+            HashSet<string> b = new HashSet<string>();
+            foreach (Song s in playlist2.Songs)
+            {
+                b.Add(s.Name);
+            }
+
+
+            return a.IsSubsetOf(b);
+        }
+
+        private static void playlistCompliment(Playlist Playlist1, Playlist Playlist2)
+        {
+            HashSet<string> Playlist1Songs = new HashSet<string>();
+            HashSet<string> Playlist2Songs = new HashSet<string>();
+
+            foreach (Song s in Playlist1.Songs)
+            {
+                Playlist1Songs.Add(s.Name);
+            }
+
+            foreach (Song s in Playlist2.Songs)
+            {
+                Playlist2Songs.Add(s.Name);
+            }
+
+            HashSet<string> UniquetoPL = new HashSet<string>();
+
+
+            //Finds Songs in setA which compliment SetB 
+            UniquetoPL = new HashSet<string>(Playlist1Songs.Except(Playlist2Songs));
+
+            Console.WriteLine("These songs are unique to the first playlist: ");
+
+            Console.Write("{");
+            foreach (string s in UniquetoPL)
+            {
+                Console.Write(" {0},", s);
+            }
+            Console.WriteLine(" }");
+
+            //Finds Songs in setB which compliment SetA 
+            UniquetoPL = new HashSet<string>(Playlist2Songs.Except(Playlist1Songs));
+
+            Console.WriteLine("These songs are unique to the second playlist: ");
+            Console.Write("{");
+            foreach (string s in UniquetoPL)
+            {
+                Console.Write(" {0},", s);
+            }
+            Console.WriteLine(" }");
         }
     }
 }
